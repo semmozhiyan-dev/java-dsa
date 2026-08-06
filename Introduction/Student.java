c314b6f869cd338ae7a463251f6f2f2fd6cdd0cf
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-06 14:15:47 +0530

    feat:implement basic student data were getting the input from user

---
+++ Introduction/Student.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Student{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter name: " );
+        String name =sc.nextLine();
+        System.out.println("enter the roll number: ");
+        int num =sc.nextInt();
+        System.out.println("enter the age: ");
+        int age =sc.nextInt();
+        System.out.println("enter the department: ");
+        String department =sc.next();
+        System.out.println("entered name: "+name);
+        System.out.println("entered roll number: "+num);
+        System.out.println("entered age: "+age);    
+        System.out.println("entered department: "+department);
+    }
+}
