55094f93aba662fe25a9c52a3b3af0d14a8ff481
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-07 10:02:01 +0530

    feat:implement nested if condition

--- Introduction/Condn.java
+++ Introduction/Condn.java
@@ -1,13 +1,19 @@
 public class Condn {
     public static void main(String[] args) {
-        int age=30;
+        int age=70;
         //true
         if(age>=18){
             System.out.println("Adult");
+            if(age>=60){
+            System.out.println("old");
         }
+        }        
         //false
         else{
             System.out.println("Minor");
+            if(age<=10){
+                System.out.println("child");
+            }
         }
     }    
 }
