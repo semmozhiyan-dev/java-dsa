e9c07c25c11a675208a3c2cc93647ae2bf06a65e
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-07 10:30:46 +0530

    feat:implement if else if condition

--- Introduction/Condn.java
+++ Introduction/Condn.java
@@ -1,5 +1,6 @@
 public class Condn {
     public static void main(String[] args) {
+        /* 
         int age=70;
         //true
         if(age>=18){
@@ -15,5 +16,36 @@ public class Condn {
                 System.out.println("child");
             }
         }
+            */
+           //nested if else
+        int age=70;
+        if(age<=3){
+            System.out.println("baby");
+        }else if(age>3 && age<=12){
+            System.out.println("child");
+        }else if(age>12 && age<=18){
+            System.out.println("teenager");
+        }else if(age>19 && age<=60){
+            System.out.println("adult");
+        }else{
+            System.out.println("old");
+        }  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }    
 }
