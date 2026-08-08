81ad75c5faf349aa6e9fc3d7669beed3c092e92b
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-08 10:10:49 +0530

    feat:implement for looping

---
+++ Introduction/Looping.java
@@ -0,0 +1,8 @@
+public class Looping{
+    public static void main(String[] args){
+        int n=10;
+        for(int i=1;i<=n;i++){
+            System.out.println("Hello World");
+        }
+    }
+}
