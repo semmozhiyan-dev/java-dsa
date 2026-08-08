715d0e72cdbb46a749970b5e56200f8e2f378e2c
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-08 11:14:37 +0530

    feat:add nested for loop

--- Introduction/Looping.java
+++ Introduction/Looping.java
@@ -4,14 +4,19 @@ public class Looping{
         for(int i=1;i<=n;i++){
             System.out.println("Hello World");
         }*/
-       int n=10;
+       /*int n=10;
        int sum=0;
        for(int i=1;i<=n;i++){
            sum=sum+i;
        }
        System.out.println("Sum: " + sum);
-
+*/
+       for(int i=1;i<=5;i++){
+           for(int j=1;j<=4;j++){
+               System.out.println("i= "+i+ " j= "+j);
+           }
+           System.out.println();
         }
 
-
+    }
 }
