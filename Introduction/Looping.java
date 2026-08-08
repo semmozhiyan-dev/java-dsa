79551d241b69bf08770f3d80b2c189330788ba03
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-08 11:18:32 +0530

    feat:implement comments

--- Introduction/Looping.java
+++ Introduction/Looping.java
@@ -4,6 +4,8 @@ public class Looping{
         for(int i=1;i<=n;i++){
             System.out.println("Hello World");
         }*/
+
+            //for loop with sum of first n numbers
        /*int n=10;
        int sum=0;
        for(int i=1;i<=n;i++){
@@ -11,6 +13,9 @@ public class Looping{
        }
        System.out.println("Sum: " + sum);
 */
+
+
+//for loop with nested for loop
        for(int i=1;i<=5;i++){
            for(int j=1;j<=4;j++){
                System.out.println("i= "+i+ " j= "+j);
