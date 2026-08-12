f61e3060c27ebef8f57e89c39a646e26d4c3409e
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-12 12:00:34 +0530

    fix pattern problem

--- Introduction/Pattern.java
+++ Introduction/Pattern.java
@@ -1,11 +1,11 @@
 public class Pattern {
     public static void main(String[] args){
         int n=4;
-        for(int row=0;row<=n;row++){
+        for(int row=1;row<=n;row++){
             for(int col=1;col<=row;col++){
                 System.out.print("*");
             }
-            System.out.println("*");
+            System.out.println();
         }
 
     }
