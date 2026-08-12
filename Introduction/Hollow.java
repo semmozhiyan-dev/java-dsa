b46ab158162fc208da338a7f86a8847f8cd50f43
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-12 12:37:20 +0530

    hollow pattern

---
+++ Introduction/Hollow.java
@@ -0,0 +1,18 @@
+public class Hollow {
+    public static void main(String[] args){
+        int n=5;
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                if(row==1 || row==n || col==1 || col==n ){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+    
+}
