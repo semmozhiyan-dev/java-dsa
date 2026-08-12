c1affd7c41cac592a5d94d25c670af5aa3a9c93c
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-12 12:05:54 +0530

    reverse pattern

--- Introduction/Pattern.java
+++ Introduction/Pattern.java
@@ -1,7 +1,7 @@
 public class Pattern {
     public static void main(String[] args){
         int n=4;
-        for(int row=1;row<=n;row++){
+        for(int row=n;row>=1;row--){
             for(int col=1;col<=row;col++){
                 System.out.print("*");
             }
