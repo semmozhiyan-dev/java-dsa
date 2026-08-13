2d0628ddf67db148200b3bc3b35439718340f378
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-13 10:16:12 +0530

    array

---
+++ Introduction/ArraySum.java
@@ -0,0 +1,27 @@
+public class ArraySum {
+    public static void main(String[] args){
+        
+        /*//type 1
+        int size=5;
+        int [] nums=new int[size];
+        nums[0]=56;
+        nums[1]=67;
+        nums[2]=80;
+        nums[3]=89;
+        nums[4]=78;
+        for(int i=0;i<size;i++){
+            System.out.println(nums[i]);
+        }
+            */
+
+        //type 2
+        int marks[]={89,69,87,45,86};
+        int len =marks.length;
+        for(int i=0;i<marks.length;i++){
+            System.out.println(marks[i]);
+        }
+
+
+    }
+    
+}
