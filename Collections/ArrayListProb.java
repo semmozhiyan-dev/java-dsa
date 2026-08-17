5b51a46e497e8b31e79c2910bc4efef7673af188
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-17 10:22:59 +0530

    Refactor ArrayListProb class to correct method usage and improve comments

--- Collections/ArrayListProb.java
+++ Collections/ArrayListProb.java
@@ -6,11 +6,19 @@ public class ArrayListProb {
         //add() -> to store the element or to insert the element end of the array
         list.add("is");
         list.add("a");
-        list.addLast("Talent Guy");
-     //addFirst()-> to add the element front of ArrayList
-        list.addFirst("He");
-     
+        //addFirst()-> to add the element first of ArrayList
+        list.addFirst("Semmozhiyan");
+        //addLast()-> to add the element last of ArrayList
+        list.addLast("Talented Guy");   
+        //set-> to replace an element in a specific index position
+        list.set(3,"smart");
+        //add(index,element)-> insert an element a specific index
+        list.add(2,"also");
+        //get(index)-> to retrive an element of a specific element
+        System.out.println("retrive element are: "+ list.get(2));
+        
         System.out.println("list: "+list);
+
     }
     
 }
