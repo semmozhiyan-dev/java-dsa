9abfcec8ab965bf7deba385660e4d3f684b0ce9c
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-18 10:20:58 +0530

    add case sensitive and uppercase , lower case

--- Stringdsa/StringConcat.java
+++ Stringdsa/StringConcat.java
@@ -2,15 +2,25 @@
 
 public class StringConcat {
     public static void main(String[] args){
-        String name="java Programming";
+        String name="java programing";
         System.out.println("Length : " +name.length());
         //charAt -> to find specific character
-        char ch  =name.charAt(13);
+        char ch  =name.charAt(1);
         System.out.println("character : "+ch);
         //checking starting index and ending index  were ending index should be +1
         String subStr=name.substring(0,4);
         System.out.println("substring: " +subStr);
-
+        //to uppercase
+        System.out.println("upper case : "+name.toUpperCase());
+        //to lowercase
+        System.out.println("lower case : "+name.toLowerCase());
+        //compare
+        String str="rahul";
+        boolean isSame=str.equals("Rahul");
+        System.out.println("boolean :"+isSame);
+        boolean isSameCase=str.equalsIgnoreCase("rahul");
+        System.out.println("without case sensitive: "+isSameCase);
+        
     }
     
 }
