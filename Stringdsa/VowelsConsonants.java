82796a376d349a25d1120de491f6ba681b4d0e62
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-08-18 12:11:12 +0530

    add VowelsConsonants class to count vowels and consonants in a string

---
+++ Stringdsa/VowelsConsonants.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+public class VowelsConsonants {
+    public  static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string: ");
+        String str=sc.nextLine();
+        int vowels=0,consonants=0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+                ch=Character.toLowerCase(ch);
+                if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                    vowels++;
+                }else{
+                    consonants++;
+                }
+            }
+        }
+        System.out.println("Vowels: "+vowels);
+        System.out.println("Consonants: "+consonants);
+    }
+
+    
+}
