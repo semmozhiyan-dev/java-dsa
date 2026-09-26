9eb2ef54f769cca99459b53fab56268d2f43198d
semmoziyan-dev <semmozhiyan.dev@gmail.com>
2026-09-26 09:06:45 +0530

    add FibonacciRecursive and Polymorphism classes; update launch configuration for ternary class

---
+++ Introduction/ternary.java
@@ -0,0 +1,9 @@
+public class ternary {
+    public static void main(String[] args) {
+        int age = 13;
+        String adult = "Adult";
+        String teen = "Teen";
+        String abc =age>=18 ?adult:teen;
+        System.out.println(abc);
+    }
+}
